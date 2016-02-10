49feacc5b6e9b39167f5fa25ad8703db1c650625
lwer0 <supwer00@gmail.com>
2016-02-10 21:39:15 +0100

    Update Main.java

--- src/com/lwer0/lobbyutilities/Main.java
+++ src/com/lwer0/lobbyutilities/Main.java
@@ -1,28 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.lwer0.lobbyutilities;
 
-import java.io.File;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
 
-public class Main extends JavaPlugin {
+/**
+ *
+ * @author Joel
+ */
+class LobbyListener implements Listener {
     
-    public static Main instance;
+    private final Main plugin;
+    String sound;
+
+    public LobbyListener(Main instance) {
+        plugin = instance;
+    }
     
-    final FileConfiguration config = this.getConfig();
-    
-    @Override
-    public void onEnable() {
-        this.getLogger().info("LobbyUtils has been loaded correctly");
-        this.getServer().getPluginManager().registerEvents(new LobbyListener(instance), instance);
-        
-        File config = new File(getDataFolder()+File.separator+"config.yml");
-        if (!config.exists()) {
-            getConfig().options().copyDefaults(true);
-            saveConfig();
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPJoin(PlayerJoinEvent event) {
+        Player p = (Player) event.getPlayer();
+        if (p.isOp()) {
+            if (plugin.getConfig().getBoolean("DisableThis")) {
+                p.sendMessage("Remember to config LobbyUtilities in /plugins/LobbyUtilities");
+            }
+        }
+        if (plugin.config.getBoolean("LobbyUtils.JoinExtraMSG")) {
+            p.sendMessage(plugin.config.getString("LobbyUtils.Message.MessageExtra"));
+            if (plugin.config.getBoolean("LobbyUtils.SoundOnJoin")) {
+                sound = plugin.config.getString("LobbyUtils.Sounds.sound");
+                Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "playsound"+ " " + sound + p);
+            }
         }
     }
-    @Override
-    public void onDisable() {
-        this.getLogger().info("LobbyUtils has been unloaded correctly");
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDrop(PlayerDropItemEvent event) {
+        Player p = event.getPlayer();
+        if (!p.hasPermission("lobbyutils.nodrop.bypass")) {
+            event.setCancelled(true);
+        }
     }
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onInvClick(InventoryClickEvent event) {
+        Player p = (Player) event.getWhoClicked();
+        if (!p.hasPermission("lobbyutils.nomoveinv.bypass")) {
+            event.setCancelled(true);
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void isFlying(PlayerToggleFlightEvent event) {
+        Player p = (Player) event.getPlayer();
+        if (!p.hasPermission("lobbyutils.flykick.bypass")) {
+            if (p.isFlying()) {
+                p.kickPlayer(plugin.getConfig().getString("LobbyUtils.PlayerKickMessage"));
+            }
+        }
+    }
+    
+    @EventHandler
+    public void onBreak(BlockBreakEvent event) {
+        Player p = (Player) event.getPlayer();
+        if (!p.hasPermission("lobbyutils.build")) {
+            event.setCancelled(true);
+        }
+    }
+    
 }
